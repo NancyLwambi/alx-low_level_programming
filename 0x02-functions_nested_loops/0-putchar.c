1e5d3611d3e79d3db51864f40e99a1193d5ac03b
NancyLwambi <nancylwambi@gmail.com>
2022-03-15 03:47:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *sh = "0-putchar.c";
+while (*sh)
+{
+(*sh);
+sh++;
+}
+('\n');
+return (0);
+}
