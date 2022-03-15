0399cdda4e53e4fa6b663e087bf208c49a42e4e0
NancyLwambi <nancylwambi@gmail.com>
2022-03-15 06:49:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,9 @@
-#include "Holberton"
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-char c[] = "Putchar";
-int i;
-for (i = 0; i < 9; i++)
-{
-(c[i]);
-}
-('\n');
-return (0);
+#include <stdio.h>
+
+...
+
+printf("_putchar");
+
+...
+
 }
