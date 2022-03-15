2a6bfe0395451535377c4715fae204a9d566dac0
NancyLwambi <nancylwambi@gmail.com>
2022-03-15 07:25:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,17 @@
+#include  "main.h"
 #include <stdio.h>
-
-...
-
-printf("_putchar");
-
-...
-
+/**
+ * main - prints the string "Putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+char c[] = "Putchar";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
