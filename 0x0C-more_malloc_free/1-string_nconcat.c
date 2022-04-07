9b1bc94f4adf84ee9d6cb5e45cd3db1af4d5508c
NancyLwambi <nancylwambi@gmail.com>
2022-04-07 12:42:57 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,16 +21,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s2 == NULL)
 		s2 = "";
+
 	for (i = 0; s1[i]; i++)
 		len++;
+
 	concat = malloc(sizeof(char) * (len + 1));
+
 	if (concat == NULL)
 		return (NULL);
+
 	len = 0;
+
 	for (i = 0; s1[i]; i++)
 		concat[len++] = s1[i];
+
 	for (i = 0; s2[i] && i < n; i++)
 		concat[len++] = s2[i];
+
 	concat[len] = '\0';
+
 	return (concat);
 }
